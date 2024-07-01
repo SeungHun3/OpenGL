7b28e452f39711120e8c43ca804d18de9f08f587
Seunghun <shlee4508@gmail.com>
2024-07-01 11:42:54 +0900

    사각형 그리기

--- src/context.cpp
+++ src/context.cpp
@@ -10,18 +10,15 @@ ContextUPtr Context::Create()
 
 bool Context::Init()
 {
-    float vertices[] = {
-        -0.5f,
-        -0.5f,
-        0.0f,
-
-        0.5f,
-        -0.5f,
-        0.0f,
-
-        0.0f,
-        0.5f,
-        0.0f,
+	float vertices[] = {
+    // first triangle
+    0.5f, 0.5f, 0.0f, // top right
+    0.5f, -0.5f, 0.0f, // bottom right
+    -0.5f, 0.5f, 0.0f, // top left
+    // second triangle
+    0.5f, -0.5f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, // bottom left
+    -0.5f, 0.5f, 0.0f // top left
     };
 
     // vertexObject 생성
@@ -30,7 +27,7 @@ bool Context::Init()
 
     glGenBuffers(1, &m_vertexBuffer);              // 버퍼생성
     glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer); // GL_ARRAY_BUFFER버퍼 바인딩
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 9, vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 18, vertices, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0); // vertex attribute 중 n번째를 사용하도록 설정
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, 0);// 사용하도록 한 n번째 attribute에 값을 사용하기위한 정의
@@ -57,5 +54,5 @@ void Context::Render()
     glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(m_program->Get());
-    glDrawArrays(GL_TRIANGLES, 0, 3); // 정점 3개로 삼각형 그리기
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 }
