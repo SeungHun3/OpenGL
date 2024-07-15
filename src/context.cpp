dec4c74682bd024280b23c6c1a4ca288788159f9
Seunghun <shlee4508@gmail.com>
2024-07-15 18:43:47 +0900

    오브젝트 로더 구현 model 클래스 생성

--- src/context.cpp
+++ src/context.cpp
@@ -14,6 +14,10 @@ bool Context::Init()
 {
     m_box = Mesh::CreateBox();
 
+    m_model = Model::Load("./model/backpack.obj");
+    if (!m_model)
+        return false;
+
     m_simpleProgram = Program::Create("./shader/simple.vs", "./shader/simple.fs");
     if (!m_simpleProgram)
         return false;
@@ -27,7 +31,7 @@ bool Context::Init()
 
     // 이미지 생성
     auto image = Image::Load("./image/container.jpg");
-    if (!image) 
+    if (!image)
         return false;
     SPDLOG_INFO("image: {}x{}, {} channels", image->GetWidth(), image->GetHeight(), image->GetChannelCount());
 
@@ -35,28 +39,32 @@ bool Context::Init()
 
     auto image2 = Image::Load("./image/awesomeface.png");
     m_texture2 = Texture::CreateFromImage(image2.get());
-    m_material.diffuse = Texture::CreateFromImage(Image::Load("./image/container2.png").get());
-	m_material.specular = Texture::CreateFromImage(Image::Load("./image/container2_specular.png").get());
-    
-    glActiveTexture(GL_TEXTURE0);                       // 0번슬롯(GL_TEXTURE0) 선택 후
-    glBindTexture(GL_TEXTURE_2D, m_texture->Get());     // texture id (m_texture->Get())에 텍스처 오브젝트를 바인딩
-    
-    glActiveTexture(GL_TEXTURE1);                    // 1번슬롯(GL_TEXTURE1) 선택 후    
+    // m_material.diffuse = Texture::CreateFromImage(Image::Load("./image/container2.png").get());
+    // m_material.specular = Texture::CreateFromImage(Image::Load("./image/container2_specular.png").get());
+    m_material.diffuse = Texture::CreateFromImage(
+        Image::CreateSingleColorImage(4, 4, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f)).get());
+    m_material.specular = Texture::CreateFromImage(
+        Image::CreateSingleColorImage(4, 4, glm::vec4(0.5f, 0.5f, 0.5f, 1.0f)).get());
+
+    glActiveTexture(GL_TEXTURE0);                   // 0번슬롯(GL_TEXTURE0) 선택 후
+    glBindTexture(GL_TEXTURE_2D, m_texture->Get()); // texture id (m_texture->Get())에 텍스처 오브젝트를 바인딩
+
+    glActiveTexture(GL_TEXTURE1);                    // 1번슬롯(GL_TEXTURE1) 선택 후
     glBindTexture(GL_TEXTURE_2D, m_texture2->Get()); // 바인딩
 
     m_program->Use();
 
-    m_program->SetUniform("tex", 0);    // 0번(GL_TEXTURE0) 슬롯에 tex를 사용 
-    m_program->SetUniform("tex2", 1);   // 1번(GL_TEXTURE1) 슬롯에 tex2를 사용 
+    m_program->SetUniform("tex", 0);    // 0번(GL_TEXTURE0) 슬롯에 tex를 사용
+    m_program->SetUniform("tex2", 1);   // 1번(GL_TEXTURE1) 슬롯에 tex2를 사용
 
     return true;
 }
 
 void Context::Render()
 {
-    if (ImGui::Begin("ui window")) 
+    if (ImGui::Begin("ui window"))
     {
-        if (ImGui::ColorEdit4("clear color", glm::value_ptr(m_clearColor))) 
+        if (ImGui::ColorEdit4("clear color", glm::value_ptr(m_clearColor)))
         {
             glClearColor(m_clearColor.r, m_clearColor.g, m_clearColor.b, m_clearColor.a);
         }
@@ -65,13 +73,13 @@ void Context::Render()
         ImGui::DragFloat("camera yaw", &m_cameraYaw, 0.5f);
         ImGui::DragFloat("camera pitch", &m_cameraPitch, 0.5f, -89.0f, 89.0f);
         ImGui::Separator();
-        if (ImGui::Button("reset camera")) 
+        if (ImGui::Button("reset camera"))
         {
             m_cameraYaw = 0.0f;
             m_cameraPitch = 0.0f;
             m_cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
         }
-        if (ImGui::CollapsingHeader("light", ImGuiTreeNodeFlags_DefaultOpen)) 
+        if (ImGui::CollapsingHeader("light", ImGuiTreeNodeFlags_DefaultOpen))
         {
             ImGui::DragFloat3("l.position", glm::value_ptr(m_light.position), 0.01f);
             ImGui::DragFloat3("l.direction", glm::value_ptr(m_light.direction), 0.01f);
@@ -80,46 +88,33 @@ void Context::Render()
             ImGui::ColorEdit3("l.ambient", glm::value_ptr(m_light.ambient));
             ImGui::ColorEdit3("l.diffuse", glm::value_ptr(m_light.diffuse));
             ImGui::ColorEdit3("l.specular", glm::value_ptr(m_light.specular));
-        } 
-        if (ImGui::CollapsingHeader("material", ImGuiTreeNodeFlags_DefaultOpen)) 
+        }
+        if (ImGui::CollapsingHeader("material", ImGuiTreeNodeFlags_DefaultOpen))
         {
-            ImGui::DragFloat("m.shininess", &m_material.shininess, 1.0f, 1.0f, 256.0f);        
+            ImGui::DragFloat("m.shininess", &m_material.shininess, 1.0f, 1.0f, 256.0f);
         }
 
         ImGui::Checkbox("animation", &m_animation);
     }
     ImGui::End();
 
-    std::vector<glm::vec3> cubePositions = {
-        glm::vec3( 0.0f, 0.0f, 0.0f),
-        glm::vec3( 2.0f, 5.0f, -15.0f),
-        glm::vec3(-1.5f, -2.2f, -2.5f),
-        glm::vec3(-3.8f, -2.0f, -12.3f),
-        glm::vec3( 2.4f, -0.4f, -3.5f),
-        glm::vec3(-1.7f, 3.0f, -7.5f),
-        glm::vec3( 1.3f, -2.0f, -2.5f),
-        glm::vec3( 1.5f, 2.0f, -2.5f),
-        glm::vec3( 1.5f, 0.2f, -1.5f),
-        glm::vec3(-1.3f, 1.0f, -1.5f),
-    };
-
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
-    
+
     m_program->Use();
 
     auto projection = glm::perspective(glm::radians(45.0f), (float)m_width / (float)m_height, 0.01f, 20.0f);
 
-    m_cameraFront = 
+    m_cameraFront =
         // 기본방향을 정해주고
-        glm::vec4(0.0f, 0.0f, -1.0f, 0.0f) * 
+        glm::vec4(0.0f, 0.0f, -1.0f, 0.0f) *
         // y축으로 yaw만큼 회전시킨 벡터를
         glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraYaw), glm::vec3(0.0f, 1.0f, 0.0f)) *
         // 다시 x축으로 pitch만큼 회전
         glm::rotate(glm::mat4(1.0f), glm::radians(m_cameraPitch), glm::vec3(1.0f, 0.0f, 0.0f));
 
 
-	auto view = glm::lookAt(
+    auto view = glm::lookAt(
         m_cameraPos,
         m_cameraPos + m_cameraFront,
         m_cameraUp);
@@ -128,19 +123,19 @@ void Context::Render()
     auto lightModelTransform =
         glm::translate(glm::mat4(1.0), m_light.position) *
         glm::scale(glm::mat4(1.0), glm::vec3(0.1f));
-    
+
     // light cube
     m_simpleProgram->Use();
     m_simpleProgram->SetUniform("color", glm::vec4(m_light.ambient + m_light.diffuse, 1.0f));
     m_simpleProgram->SetUniform("transform", projection * view * lightModelTransform);
     m_box->Draw();
-    
+
     m_program->Use();
     m_program->SetUniform("viewPos", m_cameraPos);
     m_program->SetUniform("light.position", m_light.position);
     m_program->SetUniform("light.direction", m_light.direction);
     m_program->SetUniform("light.cutoff",  glm::vec2(
-        cosf(glm::radians(m_light.cutoff[0])), 
+        cosf(glm::radians(m_light.cutoff[0])),
         cosf(glm::radians(m_light.cutoff[0] + m_light.cutoff[1]))));
     m_program->SetUniform("light.attenuation", GetAttenuationCoeff(m_light.distance));
     m_program->SetUniform("light.ambient", m_light.ambient);
@@ -155,23 +150,14 @@ void Context::Render()
     glActiveTexture(GL_TEXTURE1);
     m_material.specular->Bind();
 
-    for (size_t i = 0; i < cubePositions.size(); i++)
-    {
-        auto& pos = cubePositions[i];
-        auto model = glm::translate(glm::mat4(1.0f), pos);
-        model = glm::rotate(
-            model
-            , glm::radians((m_animation ? (float)glfwGetTime() : 0.0f) * 120.0f + 20.0f * (float)i)
-            , glm::vec3(1.0f, 0.5f, 0.0f));
-             
-        auto transform = projection * view * model;
-        m_program->SetUniform("transform", transform);
-        m_program->SetUniform("modelTransform", model);
-        m_box->Draw();
-    }
+    auto modelTransform = glm::mat4(1.0f);
+    auto transform = projection * view * modelTransform;
+    m_program->SetUniform("transform", transform);
+    m_program->SetUniform("modelTransform", modelTransform);
+    m_model->Draw();
 }
 
-void Context::ProcessInput(GLFWwindow* window) 
+void Context::ProcessInput(GLFWwindow* window)
 {
     if (!m_cameraControl)
         return;
@@ -186,7 +172,7 @@ void Context::ProcessInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         m_cameraPos += cameraSpeed * cameraRight;
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        m_cameraPos -= cameraSpeed * cameraRight;    
+        m_cameraPos -= cameraSpeed * cameraRight;
 
     auto cameraUp = glm::normalize(glm::cross(-m_cameraFront, cameraRight));
     if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
@@ -195,7 +181,7 @@ void Context::ProcessInput(GLFWwindow* window)
         m_cameraPos -= cameraSpeed * cameraUp;
 }
 
-void Context::MouseMove(double x, double y) 
+void Context::MouseMove(double x, double y)
 {
     if (!m_cameraControl)
         return;
@@ -212,25 +198,27 @@ void Context::MouseMove(double x, double y)
     if (m_cameraPitch > 89.0f)  m_cameraPitch = 89.0f;
     if (m_cameraPitch < -89.0f) m_cameraPitch = -89.0f;
 
-    m_prevMousePos = pos;    
+    m_prevMousePos = pos;
 }
 
-void Context::MouseButton(int button, int action, double x, double y) 
+void Context::MouseButton(int button, int action, double x, double y)
 {
-    if (button == GLFW_MOUSE_BUTTON_RIGHT) 
+    if (button == GLFW_MOUSE_BUTTON_RIGHT)
     {
-        if (action == GLFW_PRESS) {
-          // 마우스 조작 시작 시점에 현재 마우스 커서 위치 저장
-          m_prevMousePos = glm::vec2((float)x, (float)y);
-          m_cameraControl = true;
+        if (action == GLFW_PRESS)
+        {
+            // 마우스 조작 시작 시점에 현재 마우스 커서 위치 저장
+            m_prevMousePos = glm::vec2((float)x, (float)y);
+            m_cameraControl = true;
         }
-        else if (action == GLFW_RELEASE) {
-          m_cameraControl = false;
+        else if (action == GLFW_RELEASE)
+        {
+            m_cameraControl = false;
         }
     }
 }
 
-void Context::Reshape(int width, int height) 
+void Context::Reshape(int width, int height)
 {
     m_width = width;
     m_height = height;
--- src/image.cpp
+++ src/image.cpp
@@ -10,6 +10,24 @@ ImageUPtr Image::Load(const std::string &filepath)
     return std::move(image);
 }
 
+ImageUPtr Image::CreateSingleColorImage(int width, int height, const glm::vec4 &color)
+{
+    glm::vec4 clamped = glm::clamp(color * 255.0f, 0.0f, 255.0f);
+    uint8_t rgba[4] = {
+        (uint8_t)clamped.r,
+        (uint8_t)clamped.g,
+        (uint8_t)clamped.b,
+        (uint8_t)clamped.a,
+    };
+
+    auto image = Create(width, height, 4);
+    for (int i = 0; i < width * height; i++)
+    {
+        memcpy(image->m_data + 4 * i, rgba, 4);
+    }
+    return std::move(image);
+}
+
 Image::~Image()
 {
     if (m_data)
@@ -30,7 +48,7 @@ bool Image::LoadWithStb(const std::string &filepath)
     return true;
 }
 
-ImageUPtr Image::Create(int width, int height, int channelCount) 
+ImageUPtr Image::Create(int width, int height, int channelCount)
 {
     auto image = ImageUPtr(new Image());
     if (!image->Allocate(width, height, channelCount))
@@ -38,7 +56,7 @@ ImageUPtr Image::Create(int width, int height, int channelCount)
     return std::move(image);
 }
 
-bool Image::Allocate(int width, int height, int channelCount) 
+bool Image::Allocate(int width, int height, int channelCount)
 {
     m_width = width;
     m_height = height;
@@ -47,11 +65,11 @@ bool Image::Allocate(int width, int height, int channelCount)
     return m_data ? true : false;
 }
 
-void Image::SetCheckImage(int gridX, int gridY) 
+void Image::SetCheckImage(int gridX, int gridY)
 {
-    for (int j = 0; j < m_height; j++) 
+    for (int j = 0; j < m_height; j++)
     {
-        for (int i = 0; i < m_width; i++) 
+        for (int i = 0; i < m_width; i++)
         {
             int pos = (j * m_width + i) * m_channelCount;
             bool even = ((i / gridX) + (j / gridY)) % 2 == 0;
