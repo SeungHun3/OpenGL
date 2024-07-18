1585f909ae44f0f1fc931aea8a46b5c81de2a2cd
seunghun3 <shlee4508@gmail.com>
2024-07-18 16:13:45 +0900

    Material 클래스를 사용한 여러 오브젝트 그리기

--- src/context.h
+++ src/context.h
@@ -28,7 +28,6 @@ private:
     ProgramUPtr m_simpleProgram;
 
     MeshUPtr m_box;
-    ModelUPtr m_model;
 
     TextureUPtr m_texture;
     TextureUPtr m_texture2;
@@ -42,10 +41,10 @@ private:
     // light parameter
     struct Light
     {
-        glm::vec3 position{glm::vec3(2.0f, 2.0f, 2.0f)};
+        glm::vec3 position{glm::vec3(1.0f, 4.0f, 4.0f)};
         glm::vec3 direction{glm::vec3(-1.0f, -1.0f, -1.0f)};
-        glm::vec2 cutoff{glm::vec2(20.0f, 5.0f)};
-        float distance{32.0f};
+        glm::vec2 cutoff{glm::vec2(120.0f, 5.0f)};
+        float distance{128.0f};
         glm::vec3 ambient{glm::vec3(0.1f, 0.1f, 0.1f)};
         glm::vec3 diffuse{glm::vec3(0.5f, 0.5f, 0.5f)};
         glm::vec3 specular{glm::vec3(1.0f, 1.0f, 1.0f)};
@@ -54,20 +53,16 @@ private:
     bool m_flashLightMode{false};
 
     // material parameter
-    struct Material
-    {
-        TextureUPtr diffuse;
-        TextureUPtr specular;
-        float shininess{32.0f};
-    };
-    Material m_material;
+    MaterialPtr m_planeMaterial;
+    MaterialPtr m_box1Material;
+    MaterialPtr m_box2Material;
 
     // camera parameter
     bool m_cameraControl{false};
     glm::vec2 m_prevMousePos{glm::vec2(0.0f)};
-    float m_cameraPitch{0.0f};
+    float m_cameraPitch{-20.0f};
     float m_cameraYaw{0.0f};
-    glm::vec3 m_cameraPos{glm::vec3(0.0f, 0.0f, 3.0f)};
+    glm::vec3 m_cameraPos{glm::vec3(0.0f, 2.5f, 8.0f)};
     glm::vec3 m_cameraFront{glm::vec3(0.0f, 0.0f, -1.0f)}; // 카메라가 타겟을 바라보는 방향
     glm::vec3 m_cameraUp{glm::vec3(0.0f, 1.0f, 0.0f)};
 
