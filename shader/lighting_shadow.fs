b5f0d87fa07d45592a657ac85d8ebd8123fdff41
seunghun3 <shlee4508@gmail.com>
2024-07-25 01:24:51 +0900

    shadow acne 나타나는 이유
    1.
    shadow map의 해상도는 한정적이어서 광원으로부터 먼 픽셀들은 **동일한 depth값을 샘플링**할 확률이 올라감
    2.
    정확하지 않은 depth값 때문에 줄무늬 그림자 형성
    
    해결책
    편향치(bias)를 더한 값을 비교 수치로 사용

--- shader/lighting_shadow.fs
+++ shader/lighting_shadow.fs
@@ -50,7 +50,9 @@ float ShadowCalculation(vec4 fragPosLight)
     // get depth of current fragment from light’s perspective
     float currentDepth = projCoords.z;
     // check whether current frag pos is in shadow
-    float shadow = currentDepth > closestDepth ? 1.0 : 0.0;
+    float bias = 0.005;
+    float shadow = currentDepth - bias > closestDepth ? 1.0 : 0.0;
+    
     return shadow;
 }
 
