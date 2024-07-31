d73d5a13b6aa50d10dcb59166782b948bb7dd043
Seunghun <shlee4508@gmail.com>
2024-07-31 11:13:58 +0900

    Deferred Shading : light 계산과 관련된 모든 연산을 지연시켜 렌더링
    Geometry pass 단계에서 G-buffer 만들기

--- src/framebuffer.h
+++ src/framebuffer.h
@@ -4,23 +4,25 @@
 #include "texture.h"
 
 CLASS_PTR(Framebuffer);
-class Framebuffer {
+class Framebuffer
+{
 public:
-    static FramebufferUPtr Create(const TexturePtr colorAttachment);
+    static FramebufferUPtr Create(const std::vector<TexturePtr> &colorAttachments);
     static void BindToDefault();
     ~Framebuffer();
 
     const uint32_t Get() const { return m_framebuffer; }
     void Bind() const;
-    const TexturePtr GetColorAttachment() const { return m_colorAttachment; }
+    const TexturePtr GetColorAttachment(int index = 0) const { return m_colorAttachments[index]; }
+    int GetColorAttachmentCount() const { return (int)m_colorAttachments.size(); }
 
 private:
     Framebuffer() {}
-    bool InitWithColorAttachment(const TexturePtr colorAttachment);
+    bool InitWithColorAttachments(const std::vector<TexturePtr> &colorAttachments);
 
-    uint32_t m_framebuffer { 0 };
-    uint32_t m_depthStencilBuffer { 0 };
-    TexturePtr m_colorAttachment;
+    uint32_t m_framebuffer{0};
+    uint32_t m_depthStencilBuffer{0};
+    std::vector<TexturePtr> m_colorAttachments;
 };
 
 #endif // __FRAMEBUFFER_H__
