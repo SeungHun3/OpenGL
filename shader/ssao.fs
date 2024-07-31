90fa4a2ba8d137849f78f732fd21921d128b6d87
Seunghun <shlee4508@gmail.com>
2024-07-31 17:05:35 +0900

    SSAO : view space에 따른 TBN적용 및 세부 조정 적용

--- shader/ssao.fs
+++ shader/ssao.fs
@@ -9,8 +9,15 @@ uniform sampler2D gNormal;
 uniform sampler2D texNoise;
 
 uniform mat4 view;
+uniform mat4 projection;
 
 uniform vec2 noiseScale;
+uniform float radius;
+
+const int KERNEL_SIZE = 64;
+const float BIAS = 0.025;
+uniform vec3 samples[KERNEL_SIZE];
+
 
 void main() 
 {
@@ -28,5 +35,25 @@ void main()
     vec3 bitangent = cross(normal, tangent);
     mat3 TBN = mat3(tangent, bitangent, normal);
     
-    fragColor = tangent.x;
+    float occlusion = 0.0;
+    for (int i = 0; i < KERNEL_SIZE; i++) 
+    {
+        vec3 sample = fragPos + TBN * samples[i] * radius;
+        vec4 screenSample = projection * vec4(sample, 1.0);
+        screenSample.xyz /= screenSample.w;
+        screenSample.xyz = screenSample.xyz * 0.5 + 0.5;
+
+        float sampleDepth = (view * texture(gPosition, screenSample.xy)).z;
+        float rangeCheck = smoothstep(0.0, 1.0, radius / abs(fragPos.z - sampleDepth));
+        occlusion += (sampleDepth >= sample.z + BIAS ? 1.0 : 0.0) * rangeCheck;
+    }
+
+    fragColor = 1.0 - occlusion / KERNEL_SIZE;
 }
+/*
+samples 벡터를 랜덤하게 회전된 TBN 행렬 및 현재 픽셀의 3D 위치를 기준으로 변환
+해당 sample의 screen 상에서의 xy값을 계산
+해당 xy값을 바탕으로 그 위치에 이미 그려진 픽셀의 depth값을 계산
+이미 그려진 depth값이 샘플 위치의 depth값보다 크다면 차폐가 발생한 것
+설정한 radius보다 멀리 떨어진 샘플이면 영향을 덜 받도록 함
+*/
