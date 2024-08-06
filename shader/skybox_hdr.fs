6595f35251fdf412d4981669839f89f338a0c939
Seunghun <shlee4508@gmail.com>
2024-08-06 11:08:39 +0900

    IBL Reflectance, specular 적용 environment map과 BRDF lookup table을 PBR 렌더링에 전달

--- shader/skybox_hdr.fs
+++ shader/skybox_hdr.fs
@@ -4,12 +4,10 @@ out vec4 fragColor;
 in vec3 localPos;
 
 uniform samplerCube cubeMap;
-uniform float roughness;
 
 void main() 
 {
-    // 모든 mip level의 결과 확인용 임시 수정
-    vec3 envColor = textureLod(cubeMap, localPos, roughness * 4).rgb;
+    vec3 envColor = texture(cubeMap, localPos).rgb;
     envColor = envColor / (envColor + vec3(1.0));   // reinhard
     envColor = pow(envColor, vec3(1.0/2.2));    // to sRGB
     fragColor = vec4(envColor, 1.0);
