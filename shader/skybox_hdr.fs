9b5950db195f10447d49fd2a815002a82403f610
Seunghun <shlee4508@gmail.com>
2024-08-06 10:40:57 +0900

    PBR mipmap 적용

--- shader/skybox_hdr.fs
+++ shader/skybox_hdr.fs
@@ -4,10 +4,12 @@ out vec4 fragColor;
 in vec3 localPos;
 
 uniform samplerCube cubeMap;
+uniform float roughness;
 
 void main() 
 {
-    vec3 envColor = texture(cubeMap, localPos).rgb;
+    // 모든 mip level의 결과 확인용 임시 수정
+    vec3 envColor = textureLod(cubeMap, localPos, roughness * 4).rgb;
     envColor = envColor / (envColor + vec3(1.0));   // reinhard
     envColor = pow(envColor, vec3(1.0/2.2));    // to sRGB
     fragColor = vec4(envColor, 1.0);
